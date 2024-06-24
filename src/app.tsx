10b653d46674c2dfa29a4e69aa1395c11f6d16e4
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-06-24 14:56:05 +0530

    feat: zerops setup

--- src/app.tsx
+++ src/app.tsx
@@ -9,9 +9,8 @@ export default function App() {
     <Router
       root={props => (
         <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
+          <Title>Solid Nodejs with Zerops</Title>
+          <a href="/"></a>
           <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
